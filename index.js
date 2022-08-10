865614dcd0d58ab3818ccf65e72d256532a51bbe
TMysiv <tarasmysiv@gmail.com>
2022-08-10 17:50:02 +0300

    idea

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const TelegramApi = require('node-telegram-bot-api');
+const {againOptions,gameOptions} = require('./options')
 
 const token = '5434639318:AAEnqf6m_rpzdStEIX6ecYikm6VxBjtVc-g';
 
@@ -13,24 +14,6 @@ const startGame = async (chatId) => {
     await bot.sendMessage(chatId, 'Відгадуй', gameOptions)
 }
 
-const gameOptions = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [
-            [{text: '1', callback_data: '1'},{text: '2', callback_data: '2'},{text: '3', callback_data: '3'}],
-            [{text: '4', callback_data: '4'},{text: '5', callback_data: '5'},{text: '6', callback_data: '6'}],
-            [{text: '7', callback_data: '7'},{text: '8', callback_data: '8'},{text: '9', callback_data: '9'}],
-            [{text: '0', callback_data: '0'}]
-        ]
-    })
-}
-
-const againOptions = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [
-            [{text: 'Грати ще', callback_data: '/again'}]
-        ]
-    })
-}
 
 const start = () => {
     bot.setMyCommands([
